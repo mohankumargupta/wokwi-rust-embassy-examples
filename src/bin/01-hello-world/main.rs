f838291f2558e2cf706d5d2fba4a4e560dbb5fe7
Mohan Gupta <mohangupta@live.com>
2025-04-08 17:40:49 +1000

    only prints once, doesnt print continuously

--- src/bin/01-hello-world/main.rs
+++ src/bin/01-hello-world/main.rs
@@ -8,24 +8,40 @@
 #![no_std]
 #![no_main]
 
+use embassy_executor::Spawner;
+use embassy_time::{Duration, Timer};
 use esp_backtrace as _;
-use esp_hal::{clock::CpuClock, delay::Delay, main};
+use esp_hal::timer::timg::TimerGroup;
 use log::{error, info, warn};
 
-#[main]
-fn main() -> ! {
-    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-    let _peripherals = esp_hal::init(config);
+#[esp_hal_embassy::main]
+async fn main(spawner: Spawner) {
+    let peripherals = esp_hal::init(esp_hal::Config::default());
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    esp_hal_embassy::init(timg0.timer0);
+    esp_println::logger::init_logger(log::LevelFilter::Info);
+    // let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    // let _peripherals = esp_hal::init(config);
 
-    let delay = Delay::new();
+    //let delay = Delay::new();
 
     //esp_println::logger::init_logger_from_env();
-    esp_println::logger::init_logger(log::LevelFilter::Info);
-    info!("This is just info!\r");
-    warn!("This is just a warning!\r");
-    error!("Entering the danger zone!\r");
-    esp_println::println!("Garden variety println!\r");
+    info!("About to spawn hello world task!\r");
+    spawner
+        .spawn(hello_world())
+        .expect("Something went wrong spawning hello world task!");
+    info!("Just spawned hello world task!\r");
     loop {
-        delay.delay_millis(2000);
+        Timer::after(Duration::from_secs(5)).await;
+    }
+}
+
+#[embassy_executor::task]
+async fn hello_world() {
+    loop {
+        warn!("This is just a warning!\r");
+        error!("This is just an error. DANGER DANGER!\r");
+        esp_println::println!("Garden variety println!\r");
+        Timer::after(Duration::from_secs(5)).await;
     }
 }
